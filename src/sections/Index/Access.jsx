729ecbee9786afd88d68a07ade35dc8e8a77f090
Thibault Delavoux <thibault.delavoux@gmail.com>
2023-03-12 11:24:34 +0100

    refactor: convert Access to components

--- src/sections/Index/Access.jsx
+++ src/sections/Index/Access.jsx
@@ -5,9 +5,9 @@ export function Access(){
     return (
         <section>
             <Container css={{ h: '100%'}} lg>
-                <h3>Localisation</h3>
+                <Text h3>Localisation</Text>
                 <Text >
-                    20 minutes de Montbéliard – 30 minutes de Belfort
+                    20 minutes de Montbéliard - 30 minutes de Belfort
                 </Text>
                 <Image height={300} src="/img/map.jpg" />
             </Container>
